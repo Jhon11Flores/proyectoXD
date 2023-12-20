be30f2752c2a1e2527f3b20cfff096a22e34cb10
Jhon11Flores <jfloresg2uteq.edu.ec>
2023-12-19 23:54:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/holiiii/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.holiiii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import WebServices.Asynchtask;
+import WebServices.WebService;
+
+public class MainActivity extends AppCompatActivity implements Asynchtask {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void clickLogin(View v){
+        Bundle bundle = this.getIntent().getExtras();
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws= new WebService(
+                "https://revistas.uteq.edu.ec/ws/login.php?usr=cristian&pass=123",
+                datos, MainActivity.this, MainActivity.this);
+        ws.execute("GET");
+    }
+    @Override
+    public void processFinish(String result) throws JSONException {
+        TextView txtingresar = findViewById(R.id.txtingresar);
+        txtingresar.setText(result);
+    }
+}
